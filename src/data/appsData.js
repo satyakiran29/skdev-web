f1e2b2b537df1f4e4a0e89a33e284afe6ca03310
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2026-04-03 12:35:32 +0530

    Update appsData.js

--- src/data/appsData.js
+++ src/data/appsData.js
@@ -4,7 +4,7 @@
 
 // import anifyIcon from '../assets/anify_icon.png';
 import anisetIcon from '../assets/Aniset_logo.webp';
-import anisetImg from '../assets/Aniset_bg.webp';
+import anisetImg from '../assets/aniset_bg.webp';
 
 export const appsData = [
   // {
@@ -37,23 +37,14 @@ export const appsData = [
       telegramLink: 'https://t.me/skdev1'
     },
     reviews: [
-      { id: 1, user: "John Doe", rating: 4, comment: "Very useful for my daily tasks." },
-      { id: 2, user: "Elena", rating: 5, comment: "Beautiful design." }
-    ]
-  }, {
-    id: "aniset",
-    name: "Aniset",
-    // screenshot: anisetImg,
-    // screenshots: [anisetImg, anisetImg, anisetImg],
-    description: "An incredibly elegant tool set and routing app. Features privacy-focused tools without compromising design. Everything you need, right at your fingertips.",
-    shortDesc: "Elegant utility tools.",
-    icon: anisetIcon,
-    playStoreLink: "Coming Soon",
-    websiteLink: "coming soon",
-
-    reviews: [
-      { id: 1, user: "John Doe", rating: 4, comment: "Very useful for my daily tasks." },
-      { id: 2, user: "Elena", rating: 5, comment: "Beautiful design." }
+      { id: 1, user: "Google Play User", rating: 5, comment: "This widget is best in Play Store and it's a game-changer! It's sleek, customizable, and makes app discovery and management a breeze. The developer support for this widget is top-notch." },
+      { id: 2, user: "선배야쿠자", rating: 5, comment: "Recommend to anyone. Great set of widgets with totally original style and design. Just top notch developer. I hope this pack keeps on growing." },
+      { id: 3, user: "Ahmed Alkhatib", rating: 5, comment: "Using it since one year and really like it's widgets. And developer thank you for your efforts." },
+      { id: 4, user: "Gaurav Kumar", rating: 5, comment: "I'm in Love with this Kwgt & Klwp 🥹🩷 thank you so much" },
+      { id: 5, user: "Fatema", rating: 5, comment: "Awesome widgets especially the anya one looks cool" },
+      { id: 6, user: "Amartya Anand", rating: 4, comment: "Great widget and wall set. Only found 1 locked widget (I hate those). Gonna keep." },
+      { id: 7, user: "Nate Westby", rating: 5, comment: "Thanks You for making anime kwgt ❤️❤️❤️" },
+      { id: 8, user: "A Koushik", rating: 5, comment: "Good app. Nice dev." }
     ]
   }
 ];
